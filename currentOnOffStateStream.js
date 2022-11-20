1910fa17a4510eee0b417c22f3ad334ec7a6ee2e
Miguel <mig.ruiz@gmail.com>
2022-11-20 18:07:55 +0000

    onOffStream

---
+++ currentOnOffStateStream.js
@@ -0,0 +1,38 @@
+const { Observable,merge,timer, interval, of } = require('rxjs');
+const { mergeMap, first, withLatestFrom, map,share,shareReplay, filter,mapTo,take,debounceTime,throttle,throttleTime, startWith, takeWhile, delay, scan, distinct,distinctUntilChanged, tap, flatMap, takeUntil, toArray, groupBy, concatMap} = require('rxjs/operators');
+
+
+const { dayTimeStream }= require('./dayTimeStream')
+
+
+
+
+const masterSwitchSensor = new Observable(async subscriber => {  
+    var mqttCluster=await mqtt.getClusterAsync()   
+    mqttCluster.subscribeData('zigbee2mqtt/0x84ba20fffecacbc4', function(content){   
+            subscriber.next(content)
+    });
+  });
+
+  const masterSwitchStream = masterSwitchSensor.pipe(
+    filter( c=> c.action==='on' || c.action==='brightness_stop' || c.action==='brightness_move_up')
+  )
+
+
+
+
+
+const currentOnOffStream = merge(masterSwitchStream,dayTimeStream).pipe(
+    scan((acc, curr) => {
+        if (curr.action==='date_time') return {triggeredBy:'timeOfDay', lightsTurnedOn:curr.lightsTurnedOn}
+        if (curr.action==='on') return {triggeredBy:'device', lightsTurnedOn:true}
+        if (curr.action==='brightness_stop') return {triggeredBy:'device', lightsTurnedOn:false}
+        if (curr.action==='brightness_move_up') return {triggeredBy:'device', lightsTurnedOn:false }
+        
+    }, {triggeredBy:'init', lightsTurnedOn:false}),
+    share()
+)
+const lastEmissionOnOFFStream = currentOnOffStream.pipe(shareReplay(1))
+
+module.exports.currentOnOffStream =  currentOnOffStream
+module.exports.lastEmissionOnOFFStream =  lastEmissionOnOFFStream
