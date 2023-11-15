e541fe9ab269934c3360738714405407d719743c
Miguel <mruiz>
2023-11-15 10:46:52 +0000

    delay

--- App/app.js
+++ App/app.js
@@ -34,6 +34,8 @@ const combinedStream = merge(masterSwitchStream, movementStream, dayTimeStream).
 combinedStream.subscribe(async m => {
     console.log('overall', m);
     (await mqtt.getClusterAsync()).publishMessage('zigbee2mqtt/0x0c4314fffe20d4f8/set', JSON.stringify({ state: m.actionState ? "ON" : "OFF" }));
+    await new Promise(r => setTimeout(r, 500));
     (await mqtt.getClusterAsync()).publishMessage('zigbee2mqtt/0xa4c1388e3fe8b3b1/set', JSON.stringify({ state: m.actionState ? "ON" : "OFF" }));
+    await new Promise(r => setTimeout(r, 500));
     (await mqtt.getClusterAsync()).publishMessage('zigbee2mqtt/0x385b44fffee7a042/set', JSON.stringify({ state: m.actionState ? "ON" : "OFF" }));
 })
