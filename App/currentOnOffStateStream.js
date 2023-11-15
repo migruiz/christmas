7fc7fef8a698a8a15d98aa3b382aa099903d6733
Miguel <mruiz>
2023-11-15 07:36:37 +0000

    using new swithc

--- App/currentOnOffStateStream.js
+++ App/currentOnOffStateStream.js
@@ -9,17 +9,19 @@ var mqtt = require('./mqttCluster.js');
 
 const masterSwitchSensor = new Observable(async subscriber => {  
     var mqttCluster=await mqtt.getClusterAsync()   
-    mqttCluster.subscribeData('zigbee2mqtt/0x84ba', function(content){   
+    mqttCluster.subscribeData('zigbee2mqtt/0x2c1165fffecad895', function(content){   
             subscriber.next(content)
     });
   });
 
 
   const masterSwitchStream = masterSwitchSensor.pipe(
-    filter( c=> c.action==='on' || c.action==='brightness_stop' || c.action==='brightness_move_up'),
+    filter( c=> c.action==='on' || c.action==='off' || c.action==='brightness_move_down' || c.action==='brightness_move_up'),
     map((event) => {
-      if (event.action==='init') return  {type:'masterOn'}
-      if (event.action==='brightness_move_up') return  {type:'masterOff'}      
+      if (event.action==='on') return  {type:'masterOn'}
+      if (event.action==='brightness_move_up') return  {type:'masterOn'}
+      if (event.action==='off') return  {type:'masterOff'}      
+      if (event.action==='brightness_move_down') return  {type:'masterOff'}      
   }),
   )
 
