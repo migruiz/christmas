180ae3c07093688e67b99d6211e80719f4efc244
Miguel <mruiz>
2023-11-15 10:34:51 +0000

    new switches

--- App/app.js
+++ App/app.js
@@ -33,7 +33,7 @@ const combinedStream = merge(masterSwitchStream, movementStream, dayTimeStream).
 
 combinedStream.subscribe(async m => {
     console.log('overall', m);
-    //(await mqtt.getClusterAsync()).publishMessage('zigbee2mqtt/0x385b44fffee7a042/set',{state:m});
+    (await mqtt.getClusterAsync()).publishMessage('zigbee2mqtt/0x0c4314fffe20d4f8/set', JSON.stringify({ state: m.actionState ? "ON" : "OFF" }));
     (await mqtt.getClusterAsync()).publishMessage('zigbee2mqtt/0xa4c1388e3fe8b3b1/set', JSON.stringify({ state: m.actionState ? "ON" : "OFF" }));
-    //(await mqtt.getClusterAsync()).publishMessage('zigbee2mqtt/0xa4c138b23751a6d9/set',{state:m});
+    (await mqtt.getClusterAsync()).publishMessage('zigbee2mqtt/0x385b44fffee7a042/set', JSON.stringify({ state: m.actionState ? "ON" : "OFF" }));
 })
